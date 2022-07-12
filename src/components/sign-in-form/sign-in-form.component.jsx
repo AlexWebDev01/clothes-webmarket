63d805db50ac5f93627c9ca9ed5461bc75fdea83
Alex Udodov <102874212+AlexWebDev01@users.noreply.github.com>
2022-07-12 15:25:06 +0300

    observer pattern

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,9 +1,7 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
-
-import { UserContext } from '../../contexts/user.context'
 
 import { signInWithGooglePopup, createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
 
@@ -18,8 +16,6 @@ const SignInForm = () => {
     const [formFields, setFormFields] = useState(defaultformFields);
     const { email, password } = formFields;
 
-    const { setCurrentUser } = useContext(UserContext);
-
     const resetFormField = () => {
         setFormFields(defaultformFields);
     }
@@ -27,11 +23,8 @@ const SignInForm = () => {
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-            const {user} = await signInAuthUserWithEmailAndPassword(email, password);
-            setCurrentUser(user);
-            
+            await signInAuthUserWithEmailAndPassword(email, password);
             resetFormField();
-
         } catch(error) {
             switch(error.code) {
                 case 'auth/wrong-password':
@@ -53,8 +46,7 @@ const SignInForm = () => {
     }
 
     const signInWithGoogle = async () => {
-        const { user } = await signInWithGooglePopup();
-        await createUserDocumentFromAuth(user);
+        await signInWithGooglePopup();
     };
 
     return (
